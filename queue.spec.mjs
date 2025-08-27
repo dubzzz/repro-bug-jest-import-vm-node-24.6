6a343a2140a429843d3f2e6c78310ba30563d0a3
Nicolas DUBIEN <github@dubien.org>
2025-08-27 07:58:26 +0000

    more details

--- queue.spec.mjs
+++ queue.spec.mjs
@@ -1,5 +1,5 @@
 import { queue } from "./queue.mjs";
-import fc from "fast-check";
+import fc from "fast-check"; // the problem seems to be triggered by fast-check being local and not from registry
 import { jest } from "@jest/globals";
 
 test("should resolve in call order", async () => {
