aea4afef964397887c928c5c723af2b05d62b69f
Nicolas DUBIEN <github@dubien.org>
2025-08-27 07:59:32 +0000

    drop unneeded code

--- queue.spec.mjs
+++ queue.spec.mjs
@@ -2,29 +2,4 @@ import { queue } from "./queue.mjs";
 import fc from "fast-check"; // the problem seems to be triggered by fast-check being local and not from registry
 import { jest } from "@jest/globals";
 
-test("should resolve in call order", async () => {
-  await fc.assert(
-    fc.asyncProperty(
-      fc.scheduler(),
-      fc.integer({ min: 1, max: 10 }),
-      async (s, numCalls) => {
-        // Arrange
-        const pendingQueries = [];
-        const seenAnswers = [];
-        const expectedAnswers = [];
-        const call = jest.fn().mockImplementation((v) => Promise.resolve(v));
-
-        // Act
-        const queued = queue(s.scheduleFunction(call));
-        for (let id = 0; id !== numCalls; ++id) {
-          expectedAnswers.push(id);
-          pendingQueries.push(queued(id).then((v) => seenAnswers.push(v)));
-        }
-        await s.waitFor(Promise.all(pendingQueries));
-
-        // Assert
-        expect(seenAnswers).toEqual(expectedAnswers);
-      }
-    )
-  );
-});
+test("should resolve in call order", async () => {});
